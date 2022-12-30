14f0c12fc5a5a95ae5298594b308353713c3c6d4
Divya <divya5guntaka@gmail.com>
2022-12-30 19:36:40 +0530

    Update

--- env/dev.tfvars
+++ env/dev.tfvars
@@ -2,3 +2,5 @@ env = "dev"
 
 ## VPC
 vpc_cidr_block    = "10.0.0.0/16"
+subnet_cidr_block = ["10.0.0.0/17", "10.0.128.0/17"]
+subnet_azs        = ["us-east-1a", "us-east-1b"]
--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "vpc" {
   source                  = "github.com/d-devop/tf-module-vpc"
   vpc_cidr_block          = var.vpc_cidr_block
   env                     = var.env
+  subnet_azs              = var.subnet_azs
+  subnet_cidr_block       = var.subnet_cidr_block
 }
