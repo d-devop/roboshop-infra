db12d47e82e2052c8aec8d1b42d4ed2b76d978bc
Divya <divya5guntaka@gmail.com>
2023-01-04 16:01:26 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -4,3 +4,7 @@ variable "default_vpc_cidr_block" {}
 variable "default_vpc_route_table" {}
 
 variable "vpc" {}
+variable "docdb" {}
+variable "kms_key_id" {}
+variable "workstation_ip" {}
+variable "rds" {}
