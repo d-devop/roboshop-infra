1c32cc11db5124d5dda07dcb153c5bbe145a93c0
d-devop <divya5guntaka@gmail.com>
2023-01-27 19:14:15 +0530

    Update

--- main.tf
+++ main.tf
@@ -108,3 +108,14 @@ module "alb" {
 
   vpc = module.vpc
 }
+
+module "eks" {
+  source                  = "github.com/r-devops/tf-module-eks"
+  ENV                     = var.env
+  PRIVATE_SUBNET_IDS      = lookup(lookup(module.vpc, var.env, null), "app_subnets_ids", null)
+  PUBLIC_SUBNET_IDS       = lookup(lookup(module.vpc, var.env, null), "public_subnets_ids", null)
+  DESIRED_SIZE            = 1
+  MAX_SIZE                = 1
+  MIN_SIZE                = 1
+  CREATE_EXTERNAL_SECRETS = true
+}
